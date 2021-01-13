55ba2c8da7b43f3336d20f2eab0e6bd0efb146b0
Ben <blscott@gmail.com>
2021-01-13 19:01:10 +1300

    added details popup. can remove items.

--- src/index.js
+++ src/index.js
@@ -11,17 +11,19 @@ import {toDosManager, domManipulator} from "./todoFunctions.js"
 const display = document.querySelector('.main');
 const openForm = document.querySelector('.new-todo');
 const closeForm = document.querySelector('.create-new__close');
-const overlay = document.querySelector('.overlay');
+const overlayNew = document.querySelector('.overlay-new');
 const addToDoForm = document.querySelector('.create-new');
+const detailsPopup = document.querySelector('.details-popup');
+const detailsOverlay = document.querySelector('.overlay-details');
 // todo checkbox
 // const checkBoxes = document.querySelectorAll('.todo__complete');
 // array of todo items
 const todos = [];
 
-todos.push(toDosManager.createToDo("brush teef", "low", "2021-12-12"));
-todos.push(toDosManager.createToDo("sniff coke", "high", "2021-11-11"));
-todos.push(toDosManager.createToDo("scratch nutz", "high", "2021-10-30"));
-todos.push(toDosManager.createToDo("feed jimmy", "medium", "2021-06-09"));
+todos.push(toDosManager.createToDo("brush teef", "low", "2021-12-12", " with colgate", "health"));
+todos.push(toDosManager.createToDo("sniff coke", "high", "2021-11-11", "only the good columbian stuff", "lesuire"));
+todos.push(toDosManager.createToDo("scratch nutz", "high", "2021-10-30", " theyre really itchy today", "health"));
+todos.push(toDosManager.createToDo("feed jimmy", "medium", "2021-06-09", "only the finest bickies", "chores"));
 
 
 domManipulator.renderToDos(todos, display);
@@ -29,21 +31,47 @@ domManipulator.renderToDos(todos, display);
 
 
 openForm.addEventListener('click', () => {
-    overlay.classList.toggle('overlay-invisible');
+    overlayNew.classList.toggle('overlay-new-invisible');
     addToDoForm.classList.toggle('create-new-open');
 })
 
 
 
 closeForm.addEventListener('click', () => {
-    overlay.classList.toggle('overlay-invisible');
+    overlayNew.classList.toggle('overlay-new-invisible');
     addToDoForm.classList.toggle('create-new-open');
+
+    // I want the form to fade out before the inputs are reset
+    const sleep = (milliseconds) => {
+        return new Promise(resolve => setTimeout(resolve, milliseconds))
+      }
     
+    sleep(300).then(() => {
+        // clear inputs after form closes 
+        addToDoForm.reset();
+        // removes active status from all priority buttons
+        domManipulator.removeActivePriority();
+    })
 })
 
-addToDoForm.addEventListener('submit', e => toDosManager.addNewToDo(e, todos, display, overlay, addToDoForm));
+addToDoForm.addEventListener('submit', e => {
+    toDosManager.addNewToDo(e, todos, display, overlayNew, addToDoForm);
+});
 
-//2021-01-12 mm-dd-yyyy
+// will need to put this listener in the element creation script later
+const priorityBtns = document.querySelectorAll('.create-new__priority-btn');
+    priorityBtns.forEach(btn => {
+    btn.addEventListener('click', e =>{
+        domManipulator.activePriority(e);
+    });
+})
+
+// close details popup
+const closeDetails = document.querySelector('.details-popup__close');
+closeDetails.addEventListener('click', () => {
+    detailsPopup.classList.toggle("details-popup-open");
+    detailsOverlay.classList.toggle("overlay-details-invisible");
+})
 
 
 
--- src/todoFunctions.js
+++ src/todoFunctions.js
@@ -1,4 +1,5 @@
 import {format} from "date-fns"
+import { el } from "date-fns/locale";
 
 // DOM manipulation object 
 export const domManipulator = (function () {
@@ -11,12 +12,15 @@ export const domManipulator = (function () {
         
         // create a to-do element for each todo stored in the passed array 
         // and append them to the dom element supplied to the function
-        toDoList.forEach(todo => {
+        toDoList.forEach((todo, i) => {
             
             // create main body of the to-do item
             const toDoBody = document.createElement('div');
             toDoBody.classList.add('todo');
             toDoBody.classList.add(`priority-${todo.priority}`);
+            // give each to-do element a unique value that corresponds to
+            // it's data's position in the array
+            toDoBody.setAttribute('data-index', i);
             
             // create to-do item checkbox 
             const toDoCheckBox = document.createElement('div');
@@ -32,6 +36,9 @@ export const domManipulator = (function () {
             const toDoDetails = document.createElement('div');
             toDoDetails.classList.add('todo__detail');
             toDoDetails.textContent = 'details';
+            toDoDetails.addEventListener('click', (e) => {
+                renderDetails(e, toDoList);
+            })
     
             // create a to-do due date label.
             // displays a human readable representation of the date input string
@@ -43,23 +50,126 @@ export const domManipulator = (function () {
             const dateDay = format(dateObject, 'do');
             const dateFormated = `${dateMonth} ${dateDay}`;
             toDoDate.textContent = dateFormated;
+
+            // create a edit icon for the to-do item
+            const toDoEdit = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+            toDoEdit.classList.add('todo__icon-edit');
+            toDoEdit.classList.add('todo__icon');
+            // toDoEdit.addEventListener('click', e => toDosManager.deleteToDo(e, toDoList, element));
+            const use = document.createElementNS("http://www.w3.org/2000/svg", "use");
+            use.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '../img/edit.svg#icon-edit')
+            toDoEdit.appendChild(use);
             
             // create a delete icon for the to-do item
             const toDoDelete = document.createElementNS("http://www.w3.org/2000/svg", "svg");
             toDoDelete.classList.add('todo__icon');
-            const use = document.createElementNS("http://www.w3.org/2000/svg", "use");
-            use.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '../img/icons.svg#icon-bin')
-            toDoDelete.appendChild(use);
+            toDoDelete.classList.add('todo__icon-bin');
+            toDoDelete.addEventListener('click', e => toDosManager.deleteToDo(e, toDoList, element));
+            const use2 = document.createElementNS("http://www.w3.org/2000/svg", "use");
+            use2.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '../img/icons.svg#icon-bin')
+            toDoDelete.appendChild(use2);
             
             toDoBody.appendChild(toDoCheckBox);
             toDoBody.appendChild(toDoTitle);
             toDoBody.appendChild(toDoDetails);
             toDoBody.appendChild(toDoDate);
+            toDoBody.appendChild(toDoEdit);
             toDoBody.appendChild(toDoDelete);
     
             element.appendChild(toDoBody);
         })
     }
+
+    // retrieve the details for a selected to-do item and render them in a popup
+    function renderDetails(e, todos) {
+
+        const i = e.target.parentElement.dataset.index;
+        const overlay = document.querySelector('.overlay-details');
+        const display = document.querySelector('.details-popup__content');
+        const popup = document.querySelector('.details-popup');
+
+        // clear out the popup details information
+        display.innerHTML = "";
+
+        // create elements needed to build a details popup
+        // main display of popup
+        const body = document.createElement('div');
+        body.classList.add('details-popup__content');
+
+        // create title element
+        const name = document.createElement('div');
+        name.classList.add('details-popup__title');
+        name.textContent = todos[i].name;
+
+        // create project element
+        // element made up of 2 spans. title and content
+        const project = document.createElement('div');
+        project.classList.add('details-popup__project');
+        const projectTitle = document.createElement('span');
+        projectTitle.textContent = 'Project:';
+        projectTitle.classList.add('details-popup__catagory');
+        const projectContent = document.createElement('span');
+        projectContent.textContent = todos[i].project;
+        project.appendChild(projectTitle);
+        project.appendChild(projectContent);
+
+
+        // create priority element
+        const priority = document.createElement('div');
+        priority.classList.add('details-popup__priority');
+        const priorityTitle = document.createElement('span');
+        priorityTitle.textContent = "Priority:";
+        priorityTitle.classList.add('details-popup__catagory');
+        const priorityContent = document.createElement('span')
+        priorityContent.textContent = todos[i].priority;
+        priority.appendChild(priorityTitle);
+        priority.appendChild(priorityContent);
+
+         
+
+        // create date element
+        const date = document.createElement('div');
+        date.classList.add('details-popup__due');
+        const dateTitle = document.createElement('span');
+        dateTitle.textContent = 'Due Date:';
+        dateTitle.classList.add('details-popup__catagory');
+        const dateContent = document.createElement('span');
+        // display human readable date
+        const day = format(new Date(todos[i].date), 'do');
+        const month = format(new Date(todos[i].date), 'MMMM');
+        const year = format(new Date(todos[i].date), 'yyyy');
+        const formatedDate = `${day} ${month}, ${year}`;
+        dateContent.textContent = formatedDate;
+        date.appendChild(dateTitle);
+        date.appendChild(dateContent);
+        
+
+        // create details element
+        const details = document.createElement('div');
+        details.classList.add('details-popup__details');
+        const detailsTitle = document.createElement('span');
+        detailsTitle.classList.add('details-popup__details-title');
+        detailsTitle.textContent = "Details:";
+        const detailsContent = document.createElement('span');
+        detailsContent.textContent = todos[i].details;
+        details.appendChild(detailsTitle);
+        details.appendChild(detailsContent);
+
+        body.appendChild(name);
+        body.appendChild(project);
+        body.appendChild(priority);
+        body.appendChild(date);
+        body.appendChild(details);
+
+        display.appendChild(body);
+
+        // show popup
+        popup.classList.toggle("details-popup-open");
+        overlay.classList.toggle("overlay-details-invisible");
+
+
+    }
+
     // applies modified styling to each element of a checked off to-do item 
     function toggleCheckBox(e) {
 
@@ -75,28 +185,50 @@ export const domManipulator = (function () {
         toDoItems[2].classList.toggle('todo__detail-checked');
         // todo date
         toDoItems[3].classList.toggle('todo__date-checked');
-        // todo delete icon
+        // todo edit icon
         toDoItems[4].classList.toggle('todo__icon-checked');
+        // todo delete icon
+        toDoItems[5].classList.toggle('todo__icon-checked');
         
+    }
+
+    function removeActivePriority() {
+        // removes active status from all buttons
+        const btns = document.querySelectorAll('.create-new__priority-btn');
+        btns.forEach(btn => {
+            btn.classList.remove(`create-new__priority-btn--${btn.textContent.toLowerCase()}-active`)
+        })
+    }
+
+    // toggle active visual styling to priority buttons in create new to-do menu
+    function activePriority(e) {
+        // removes active status from all buttons
+        removeActivePriority();
+        // apply active status to the selected button
+        const priority = e.target.textContent.toLowerCase();
+        e.target.classList.add(`create-new__priority-btn--${priority}-active`);
     }
 
     return {
         renderToDos,
-        toggleCheckBox
+        toggleCheckBox,
+        activePriority,
+        removeActivePriority,
+        renderDetails
     };
-
-
 })();
 
 // To Do data manager 
 export const toDosManager = (function () {
 
     // To-do factory function
-    function createToDo(name, priority, date) {
+    function createToDo(name, priority, date, details, project) {
         return {
             name,
             priority,
-            date
+            date,
+            details,
+            project
         }
     }
 
@@ -108,22 +240,54 @@ export const toDosManager = (function () {
         e.preventDefault();
          
         const toDoTitle = (document.querySelector('#new-todo-title')).value;
+        const toDoDetails = (document.querySelector('#new-todo-details')).value;
         const toDoDate = (document.querySelector('#new-todo-date')).value;
         const toDoPriority = (document.querySelector('[name="priority"]:checked')).value;
-        // clear inputs after submission 
-        form.reset();
-    
-        const newToDo = createToDo(toDoTitle, toDoPriority, toDoDate);
+        
+        const newToDo = createToDo(toDoTitle, toDoPriority, toDoDate, toDoDetails);
         toDoList.push(newToDo);
         domManipulator.renderToDos(toDoList, display);
         
         // closes the form and removes the overlay after submission
-        overlay.classList.toggle('overlay-invisible');
+        overlay.classList.toggle('overlay-new-invisible');
         form.classList.toggle('create-new-open');
+
+        // I want the form to fade out before the inputs are reset
+        const sleep = (milliseconds) => {
+            return new Promise(resolve => setTimeout(resolve, milliseconds))
+          }
+        
+        sleep(300).then(() => {
+            // clear inputs after submission 
+            form.reset();
+            // removes active status from all buttons
+            domManipulator.removeActivePriority();
+        })
+    }
+
+    // removes selected to-do item from the array and re renders the display
+    function deleteToDo(e, toDoList, display) {
+        const element = e.target;
+        let i;
+        // sometimes the event target is the svg element, other times it is the use element.
+        // this ensures i get index of the to-do body item 
+        if (element.tagName === 'svg') {
+            i = element.parentElement.dataset.index;
+        } else if (element.tagName === 'use') {
+            i = element.parentElement.parentElement.dataset.index;
+        }
+        toDoList.splice(i, 1);
+        domManipulator.renderToDos(toDoList, display);
+        
+
+        // now we have the index of the dom element to be deleted,
+        // the corresponding object in the data array can now be deleted.
+
     }
 
     return {
         createToDo,
-        addNewToDo
+        addNewToDo,
+        deleteToDo
     }
 })();
